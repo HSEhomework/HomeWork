6e4b1d688b99a4e7cd0977adbd9140b68817e8bf
HSEhomework <31905166+HSEhomework@users.noreply.github.com>
2021-11-09 01:02:53 +0300

    Added comments and mark

--- script.py
+++ script.py
@@ -9,10 +9,12 @@ parser.add_argument("-m", "--mtime", help="modification time", action="store_tru
 parser.add_argument("-r", "--rename", help="rename")
 parser.add_argument("-l", "--ls", help="list all files in directory", action="store_true")
 args = parser.parse_args()
+# What if file does not exist or argument not provided? George
 print ("selected filename: ", args.file)
 stats = os.stat(args.file)
 
 if args.mtime:
+    # Time format? George
     timestamp_str = datetime.datetime.fromtimestamp(stats.st_mtime).strftime('%Y-%m-%d-%H:%M')
     print("modificated time: ", timestamp_str)
 
@@ -20,6 +22,7 @@ if args.size:
     print("size in megabytes: ", stats.st_size/1048576)
 
 if args.rename:
+    # What if file does not exist or argument not provided? George
     os.rename(args.file, args.rename)
     print("new file name is: ", args.rename)
 
@@ -27,3 +30,5 @@ if args.ls:
     rez = os.listdir(args.file) 
     for n, item in enumerate(rez):
         print(n+1, item)
+
+ # Good solution. For now 3/5 Points. Please support time format + fix comments to improve
